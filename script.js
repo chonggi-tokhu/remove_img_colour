9ce2c961505b3d84116851fa85a83742d5cfdc88
colourgrey <120172454+chonggi-tokhu@users.noreply.github.com>
2025-03-29 14:16:57 +0900

    .

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ function remove_colour(imgel, { r, g, b } = { r: 176, g: 0, b: 176 }, colour_mar
     ctx.putImageData(imgdata, 0, 0);
     return canvas;
 }  
+
+function crop_auto(imgel, { cropsize_x, cropsize_y } = { cropsize_x: 562, cropsize_y: 562 }, { looptime_x, looptime_y } = { looptime_x: 4, looptime_y: 3 }) {
+    if (!(imgel instanceof HTMLImageElement || imgel instanceof Image)) {
+        return false;
+    }
+    var myarr = [];
+    for (var i = 0; i < looptime_x; i += cropsize_x) {
+        for (var j = 0; j < looptime_y; j += cropsize_y) {
+            var canvas_pr = document.createElement("canvas");
+            var canvas = document.body.appendChild(canvas_pr);
+            canvas.width = cropsize_x;
+            canvas.height = cropsize_y;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(imgel, i, j, i + cropsize_x, j + cropsize_y);
+            myarr.push(canvas);
+        }
+    }
+    return myarr;
+}
